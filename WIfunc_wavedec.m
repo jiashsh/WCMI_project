57c14294133abfe62f3bdc90d34f4b149d181f0d
jiashsh <jssll99@126.com>
2021-11-24 14:17:52 +0800

    wavedec

---
+++ WIfunc_wavedec.m
@@ -0,0 +1,46 @@
+function WIfunc_wavedec()
+
+global handles
+
+ntrials = size(handles.matrices.actmatrix,1);
+
+if exist('wavedec.m','file')
+    if isempty(handles.matrices.actmatrix)
+        handles.matrices.actmatrix=zeros(ntrials,1);
+%         handles.matrices.wvmatrix=zeros(ntrials,1);
+    end 
+        trial_i = 1;
+        [handles.matrices.wvmatrix,handles.matrices.L] = ...
+            wavedec(handles.matrices.actmatrix(trial_i,:),...
+            handles.nscales,'haar');
+        handles.matrices.wvmatrix = ...
+            repmat(handles.matrices.wvmatrix,ntrials,1);
+        for trial_i = 2:ntrials
+            handles.matrices.wvmatrix(trial_i,:) = ...
+                wavedec(handles.matrices.actmatrix(trial_i,:),...
+                handles.nscales,'haar');
+        end
+    
+    
+
+else
+    
+    trial_i = 1;
+    [handles.matrices.wvmatrix,handles.matrices.L] = ...
+        WIfunc_fix_wavedec(handles.matrices.actmatrix(trial_i,:),...
+        handles.nscales);
+    handles.matrices.wvmatrix = ...
+        repmat(handles.matrices.wvmatrix,ntrials,1);
+    for trial_i = 2:ntrials
+        handles.matrices.wvmatrix(trial_i,:) = ...
+            WIfunc_fix_wavedec(handles.matrices.actmatrix(trial_i,:),...
+            handles.nscales);
+    end
+    
+end
+
+handles.matrices.wvmatrix_binsize = handles.binsize;
+handles.matrices.wvmatrix_nscales = handles.nscales;
+
+
+
