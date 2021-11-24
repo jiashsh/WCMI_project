b408565284b1e0a70041ce8838d6a8a9f0d40f6d
jiashsh <jssll99@126.com>
2021-11-24 14:16:26 +0800

    decoder

---
+++ WCMI_decoding.m
@@ -0,0 +1,20 @@
+clear all
+close all %,clc,
+
+% make handles global so all function will 'see' all parameters they need
+global handles 
+
+pathn='.\data\';
+name='simdata_ex1';
+load([pathn,name,'.mat']);
+load([pathn,name,'_handles.mat']) 
+
+% features and their scores selected by CMI
+features=textread([pathn,name,'_feature_ranking_6s']);
+handles.matrices.selected_wcmicoefs=features(:,2);
+
+% a LDA decoder
+WCMIfunc_decode_leaveoneout();
+
+handles.decode
+    
