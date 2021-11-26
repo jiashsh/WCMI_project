550be76d0154e373e7b5c54b7736e7438ba2632c
jiashsh <60965964+jiashsh@users.noreply.github.com>
2021-11-26 20:09:17 +0800

    Update WCMI_decoding.m

--- WCMI_decoding.m
+++ WCMI_decoding.m
@@ -1,12 +1,11 @@
 clear all
-close all %,clc,
+close all
 
 % make handles global so all function will 'see' all parameters they need
 global handles 
 
 pathn='.\data\';
-name='simdata_cell_1';
-load([pathn,name,'.mat']);
+name='simdata_cell_1'; 
 load([pathn,name,'_handles.mat']) 
 
 % features and their scores selected by CMI
@@ -18,3 +17,13 @@ WCMIfunc_decode_leaveoneout();
 
 handles.decode
     
+figure
+Ntrial=sum(handles.class_id==1);
+Nclass=handles.class_id(end);
+imagesc(handles.decode.WCMIconfusionmatrix/Ntrial);
+colorbar;
+xticks([1:1:Nclass]);
+yticks([1:1:Nclass]);
+xlabel('Actual Stimulus');
+ylabel('Predicted Stimulus');
+title('Confusion Matrix');
